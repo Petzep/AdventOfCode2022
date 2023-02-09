bbefe60347c9691d75f571f55d5cdd85629411dd
Nephtaly Aniceta <n.aniceta@outlook.com>
2023-02-09 21:30:17 +0100

    part 2 day 13 done

--- Dayz13/main.cpp
+++ Dayz13/main.cpp
@@ -209,8 +209,8 @@ void bubbleSort(QVariantList& list) {
 
 int main(int argc, char* argv[]) {
     // Get input file
-    // QFile inputFile = QFile("input.txt");
-    QFile inputFile = QFile("testInput.txt");
+     QFile inputFile = QFile("input.txt");
+    //QFile inputFile = QFile("testInput.txt");
     if(!inputFile.open(QFile::OpenModeFlag::ReadOnly | QFile::OpenModeFlag::Text))
     {
         qDebug() << "Could not open file";
