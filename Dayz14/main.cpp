18183aa795609d1755bd6c5c2108bb77a8576c64
Nephtaly Aniceta <n.aniceta@outlook.com>
2023-02-22 15:28:06 +0100

    Map drawing done

--- Dayz14/main.cpp
+++ Dayz14/main.cpp
@@ -7,33 +7,91 @@
 void drawMap(QList<QPolygon> rockFormations, QPoint start) {
     // Get the size of the map
     QRect map(start, -start);
-    for(const auto& formation : rockFormations) {
+    for(const auto& formation: rockFormations)
+    {
         auto xLimit = std::ranges::minmax_element(formation.constBegin(), formation.constEnd(), [](const QPoint& a, const QPoint& b) { return a.x() < b.x(); });
         auto yLimit = std::ranges::minmax_element(formation.constBegin(), formation.constEnd(), [](const QPoint& a, const QPoint& b) { return a.y() < b.y(); });
 
-        if(map.x() > xLimit.min->x()) {
+        if(map.x() > xLimit.min->x())
+        {
             map.setX(xLimit.min->x());
         }
-        if(map.y() > yLimit.min->y()) {
+        if(map.y() > yLimit.min->y())
+        {
             map.setY(yLimit.min->y());
         }
-        if(map.right() < xLimit.max->x()) {
+        if(map.right() < xLimit.max->x())
+        {
             map.setRight(xLimit.max->x());
         }
-        if(map.bottom() < yLimit.max->y()) {
+        if(map.bottom() < yLimit.max->y())
+        {
             map.setBottom(yLimit.max->y());
         }
     }
 
-    // Print map
+    // Create a map with air
+    QList<QList<char>> mapData(map.width(), QList<char>(map.height(), '.'));
+
+    // Draw the start
+    mapData[start.y() - map.y()][start.x() - map.x()] = '+';
     
 
-    
+    // Draw the formations in the map
+    for(const auto& formation: rockFormations)
+    {
+        for(auto pointIt = formation.constBegin(); pointIt != std::prev(formation.constEnd()); pointIt++)
+        {
+            // Get the direction
+            int xDirection = 0;
+            if((*pointIt).x() < (*std::next(pointIt)).x())
+            {
+                xDirection = 1;
+            }
+            else if((*pointIt).x() > (*std::next(pointIt)).x())
+            {
+                xDirection = -1;
+            }
+
+            int yDirection = 0;
+            if((*pointIt).y() < (*std::next(pointIt)).y())
+            {
+                yDirection = 1;
+            }
+            else if((*pointIt).y() > (*std::next(pointIt)).y())
+            {
+                yDirection = -1;
+            }
+
+            // Mark all the points between start and end
+            auto pointCount = (*pointIt - (*std::next(pointIt))).manhattanLength();
+            for(int i = 0; i < pointCount; i++)
+            {
+                int y = (*pointIt).y() - map.y() + (i * yDirection);
+                int x = (*pointIt).x() - map.x() + (i * xDirection);
+
+                // Set data
+                mapData[(*pointIt).y() - map.y() + (i * yDirection)][(*pointIt).x() - map.x() + (i * xDirection)] = '#';
+            }
+        }
+    }
+
+    // Print map
+    for(const auto& line: mapData)
+    {
+        QString mapString;
+        for(const auto& point: line)
+        {
+            mapString.append(point);
+        }
+
+        qDebug().noquote() << mapString;
+    }
 }
 
 int main(int argc, char* argv[]) {
     // Get input file
-    //QFile inputFile = QFile("input.txt");
+    // QFile inputFile = QFile("input.txt");
     QFile inputFile = QFile("testInput.txt");
     if(!inputFile.open(QFile::OpenModeFlag::ReadOnly | QFile::OpenModeFlag::Text))
     {
@@ -52,14 +110,16 @@ int main(int argc, char* argv[]) {
         }
 
         // Extract coordinates
-        auto coordinates = line.split("->");
+        auto          coordinates = line.split("->");
         QList<QPoint> rockPoints;
-        for(const auto& point : coordinates) {
+        for(const auto& point: coordinates)
+        {
             // Split into x and y
             auto xyValue = point.trimmed().split(',');
-            
+
             // Sanity check
-            if(xyValue.count() != 2) {
+            if(xyValue.count() != 2)
+            {
                 qDebug() << "Invalid input";
                 return 1;
             }
@@ -73,6 +133,6 @@ int main(int argc, char* argv[]) {
     }
 
 
-    drawMap(rockFormations, { 500, 0 });
+    drawMap(rockFormations, {500, 0});
     return 0;
 }
