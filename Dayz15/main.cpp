a09f5d078634da1ab462716a3d0d16a4defa9c94
Nephtaly Aniceta <n.aniceta@outlook.com>
2023-02-26 13:21:34 +0100

    Example complete (en alle qt versie gewijzigd, oeps)

--- Dayz15/main.cpp
+++ Dayz15/main.cpp
@@ -8,9 +8,13 @@ class Sensor : public QPoint {
     public:
         Sensor(int x, int y, int b_x, int b_y)
               : QPoint(x, y), closestBeacon(b_x, b_y){};
-        
-        int beaconDistance() {
+
+        int beaconDistance() const {
             return (QPoint(*this) - closestBeacon).manhattanLength();
+        }
+
+        QPoint beacon() const {
+            return closestBeacon;
         }
 
     private:
@@ -26,6 +30,84 @@ struct Map {
             return QPoint(field.width(), field.height()).manhattanLength();
         }
 };
+
+Map createMap(QList<Sensor> sensors) {
+    // Get the size of the map
+    QRect field;
+    int   maxDistance = 0;
+    for(const auto& sensor: sensors)
+    {
+        maxDistance = std::max(maxDistance, std::ranges::max(sensors, {}, &Sensor::beaconDistance).beaconDistance());
+
+        if(field.x() > sensor.x())
+        {
+            field.setX(sensor.x());
+        }
+        if(field.y() > sensor.y())
+        {
+            field.setY(sensor.y());
+        }
+        if(field.right() < sensor.x())
+        {
+            field.setRight(sensor.x());
+        }
+        if(field.bottom() < sensor.y())
+        {
+            field.setBottom(sensor.y());
+        }
+    }
+
+    // Extend with range
+    field.setX(field.x() - maxDistance);
+    field.setY(field.y() - maxDistance);
+    field.setRight(field.right() + maxDistance);
+    field.setBottom(field.bottom() + maxDistance);
+
+    // Create a map with air
+    QList<QList<char>> mapData(field.height(), QList<char>(field.width(), '.'));
+
+    // Draw the sensor ranges in the map
+    for(const auto& sensor: sensors)
+    {
+        for(int y = sensor.y() - sensor.beaconDistance(); y <= sensor.y() + sensor.beaconDistance(); y++)
+        {
+            for(int x = sensor.x() - sensor.beaconDistance(); x <= sensor.x() + sensor.beaconDistance(); x++)
+            {
+                if(abs((QPoint(x, y) - sensor).manhattanLength()) <= sensor.beaconDistance())
+                {
+                    mapData[y - field.y()][x - field.x()] = '#';
+                }
+            }
+        }
+    }
+
+    // Draw the sensors and beacons in the map
+    for(const auto& sensor: sensors)
+    {
+        mapData[sensor.y() - field.y()][sensor.x() - field.x()]                   = 'S';
+        mapData[sensor.beacon().y() - field.y()][sensor.beacon().x() - field.x()] = 'B';
+    }
+
+    return {field, mapData};
+}
+
+void drawMap(QList<QList<char>> mapData) {
+    // Print map
+    for(const auto& line: mapData)
+    {
+        QString mapString;
+        for(const auto& point: line)
+        {
+            mapString.append(point);
+        }
+
+        qDebug().noquote() << mapString;
+    }
+}
+
+void drawMap(Map map) {
+    drawMap(map.mapData);
+}
 
 int main(int argc, char* argv[]) {
     // Get input file
@@ -64,6 +146,15 @@ int main(int argc, char* argv[]) {
     }
 
     // Create a map for the sensors
+    auto map = createMap(sensors);
+
+    // Print map
+    drawMap(map);
+
+    // Get the "No beacon position"-count
+    int targetRow = 10;
+    auto nopeSitions = std::count_if(map.mapData[targetRow - map.field.top()].constBegin(), map.mapData[targetRow - map.field.top()].constEnd(), [](const char x) { return x == '#'; });
+    qDebug() << "Found count:" << nopeSitions;
 
     return 0;
 }
